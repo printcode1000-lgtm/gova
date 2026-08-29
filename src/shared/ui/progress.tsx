766cb2574122a8fa618fad603eddb2cbee4716e1
printcode1000-lgtm <print.code.1000@gmail.com>
2026-08-30 00:58:16 +0300

    Stage 54: require Progress identity and scope its indicator subpart

--- src/shared/ui/progress.tsx
+++ src/shared/ui/progress.tsx
@@ -4,13 +4,17 @@ import * as React from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
 
 import { cn } from '@/shared/utils';
-import { type UiDescriptor, uiAttributes } from '@asol/ui-registry-core';
+import {
+  createUiSubpartInstanceId,
+  type UiDescriptor,
+  uiAttributes,
+} from '@asol/ui-registry-core';
 
 import { uiPrimitiveAttributes } from './ui-primitive-attributes';
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { ui?: UiDescriptor }
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { ui: UiDescriptor }
 >(({ className, value, ui, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -25,7 +29,7 @@ const Progress = React.forwardRef<
       {...uiAttributes({
         uid: 'shared.ui.progress.indicator-H9wZ4b',
         id: 'shared.ui.progress.indicator',
-        instance: ui?.instance,
+        instance: createUiSubpartInstanceId(ui.uid, ui.instance, 'indicator'),
       })}
       className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
